2c0b3e8d548f82a938463d2c815a34dbac6309d6
Juan Morales <juanskate326@gmail.com>
2025-02-12 01:47:51 -0500

    change the url api llama

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const API_BACK = 'http://localhost:8787/api/bot';
-export const API_OLLAMA = 'http://localhost:11434/api'
+export const API_OLLAMA = 'http://192.99.246.129:11434/api'
+// export const API_OLLAMA = 'http://localhost:11434/api'
